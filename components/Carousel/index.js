95c448d78b86b4244e2b67be36db683118571b89
yuanx <yuanx.me@qq.com>
2023-07-29 22:03:34 +0800

    refacotr: refactor to react-native-reanimated

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -1,8 +1,19 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Animated, Easing, StyleSheet, View } from "react-native";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const DEFAULT_PAGE_INDEX = 0;
-const CARD_NUM = 5;
+const CARD_NUM = 3;
 
 const CARD_WIDTH = 300; // TODO: need from props
 const CARD_GAP = 10;
@@ -14,133 +25,151 @@ const CARD_MEDIUM_INDEX = 2;
 const CAROUSEL_WIDTH = CARD_NUM * CARD_WIDTH + CARD_LAST_INDEX * CARD_GAP;
 
 const ANIMATION_DURATION = 150;
-const TOUCH_EFFECT_LENGTH = 80;
+const TOUCH_EFFECT_LENGTH = 30;
 
 function Carousel({ renderItem: RenderItem, onPageChange }) {
-  const [isTouching, setIsTouching] = useState(false);
-  const [touchBeginX, setTouchBeginX] = useState(0);
-  const [touchX, setTouchX] = useState(0);
-  const [page, setPage] = useState(0);
   const prePage = useRef(0);
-  const touchOffset = useMemo(
-    () => touchX - touchBeginX,
-    [touchX, touchBeginX]
-  );
-  const offsetXAnim = useRef(new Animated.Value(0)).current;
+  const [page, _setPage] = useState(0);
+  const translateX = useSharedValue(0);
 
   const CARD_LIST = useRef(
     Array.from({ length: CARD_NUM }, (_, k) => {
       const gap = k !== 0 ? CARD_GAP * k : 0;
       const x = k * CARD_WIDTH + gap;
       const pageIndex = k - Math.floor(CARD_NUM / 2) + DEFAULT_PAGE_INDEX;
+      const sortIndex = k - Math.floor(CARD_NUM / 2);
       return {
         id: k,
-        sortIndex: k,
+        sortIndex,
         pageIndex,
         x,
       };
     })
   );
 
-  useEffect(() => {
-    moveCard(false);
-  }, [touchOffset]);
+  const setPage = useCallback(
+    (value) => {
+      prePage.current = page;
+      _setPage(value);
+    },
+    [page]
+  );
 
-  function onResponderGrant(e) {
-    console.log("onResponderGrant");
-    setIsTouching(true);
-    setTouchBeginX(e.nativeEvent.pageX);
-    setTouchX(e.nativeEvent.pageX);
-  }
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      console.log("onStart");
+      ctx.startX = translateX.value;
+    },
+    onActive: (event, ctx) => {
+      console.log("onActive");
+      translateX.value = ctx.startX + event.translationX;
+    },
+    onEnd: (event, ctx) => {
+      console.log("onEnd", event, ctx);
+      const diff = event.translationX;
+      if (diff >= TOUCH_EFFECT_LENGTH) {
+        console.log("last page");
+        runOnJS(setPage)(page - 1);
+        return;
+      } else if (diff <= -TOUCH_EFFECT_LENGTH) {
+        console.log("next page");
+        runOnJS(setPage)(page + 1);
+        return;
+      }
+      translateX.value = 0;
+    },
+  });
 
-  function onResponderMove(e) {
-    console.log("onResponderMove");
-    setTouchX(e.nativeEvent.pageX);
-  }
-
-  function moveCard(changePage, isNextPage) {
-    if (changePage) {
-      CARD_LIST.current = CARD_LIST.current.map(
-        ({ sortIndex, ...cardItem }) => {
-          let nextSortIndex =
-            sortIndex === CARD_LAST_INDEX ? CARD_FIRST_INDEX : sortIndex + 1;
-          if (isNextPage) {
-            nextSortIndex =
-              sortIndex === CARD_FIRST_INDEX ? CARD_LAST_INDEX : sortIndex - 1;
-          }
-          return {
-            ...cardItem,
-            sortIndex: nextSortIndex,
-            x: (CARD_WIDTH + CARD_GAP) * nextSortIndex,
-          };
-        }
-      );
-    }
-
-    const duration = isTouching ? 0 : ANIMATION_DURATION;
-    const pageOffset = changePage ? (isNextPage ? CARD_WIDTH : -CARD_WIDTH) : 0;
-    Animated.timing(offsetXAnim, {
-      easing: Easing.linear,
-      toValue: touchOffset + pageOffset,
-      duration,
-      useNativeDriver: false,
-    }).start();
-  }
-
-  function onResponderEnd(e) {
-    console.log("onResponderEnd");
-    setIsTouching(false);
-    setTouchBeginX(0);
-    setTouchX(0);
-    if (touchOffset > TOUCH_EFFECT_LENGTH) {
-      toLastPage();
-    } else if (touchOffset < -TOUCH_EFFECT_LENGTH) {
-      toNextPage();
-    }
-  }
-
-  function toLastPage() {
-    prePage.current = page;
-    setPage(page - 1);
-    moveCard(true, false);
-    onPageChange(prePage.current, page - 1);
-  }
-
-  function toNextPage() {
-    prePage.current = page;
-    setPage(page + 1);
-    moveCard(true, true);
-    onPageChange(prePage.current, page + 1);
-  }
   return (
-    <View style={styles.carousel}>
-      {CARD_LIST.current.map(({ id, sortIndex, pageIndex, x }) => {
-        // TODO: split to a component
-        const currentPage = useMemo(
-          () => page + sortIndex - CARD_MEDIUM_INDEX,
-          [page, sortIndex, CARD_MEDIUM_INDEX]
-        );
-        return (
-          <Animated.View
-            key={sortIndex}
-            style={[
-              styles.page,
-              {
-                left: Animated.add(offsetXAnim, x),
-              },
-            ]}
-            onStartShouldSetResponder={() => true}
-            onMoveShouldSetResponder={() => true}
-            onResponderGrant={onResponderGrant}
-            onResponderMove={onResponderMove}
-            onResponderEnd={onResponderEnd}
-          >
-            {/* <Text style={styles.text}>ID {id}</Text> */}
-            <RenderItem pageIndex={currentPage} />
-          </Animated.View>
-        );
-      })}
-    </View>
+    <GestureHandlerRootView
+      style={{
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={styles.carousel}>
+          {CARD_LIST.current.map(
+            ({ id, pageIndex, sortIndex: defaultSortIndex, x: defaultX }) => {
+              const x = useSharedValue(defaultX);
+              const offset = useSharedValue(0);
+
+              const sortIndex = useMemo(() => {
+                if (page > 0) {
+                  const index = page % CARD_NUM;
+                  if (id === 0) {
+                    return [-1, 1, 0][index];
+                  } else if (id === 1) {
+                    return [0, -1, 1][index];
+                  } else if (id === 2) {
+                    return [1, 0, -1][index];
+                  }
+                } else if (page < 0) {
+                  const index = Math.abs(page) % CARD_NUM;
+                  if (id === 0) {
+                    return [-1, 0, 1][index];
+                  } else if (id === 1) {
+                    return [0, 1, -1][index];
+                  } else if (id === 2) {
+                    return [1, -1, 0][index];
+                  }
+                }
+                return defaultSortIndex;
+              }, [page]);
+
+              useEffect(() => {
+                if (
+                  prePage.current < page &&
+                  sortIndex === Math.floor(CARD_NUM / 2)
+                ) {
+                  x.value = (sortIndex + 1) * (CARD_WIDTH + CARD_GAP);
+                  offset.value = CARD_WIDTH + CARD_GAP;
+                  offset.value = withTiming(0);
+                } else if (
+                  prePage.current > page &&
+                  sortIndex === -Math.floor(CARD_NUM / 2)
+                ) {
+                  x.value = (sortIndex + 1) * (CARD_WIDTH + CARD_GAP);
+                  offset.value = -CARD_WIDTH - CARD_GAP;
+                  offset.value = withTiming(0);
+                } else {
+                  x.value = withTiming(
+                    (sortIndex + 1) * (CARD_WIDTH + CARD_GAP)
+                  );
+                  offset.value = 0;
+                }
+                translateX.value = withTiming(0);
+              }, [sortIndex]);
+
+              const currentPage = useMemo(
+                () => page + sortIndex,
+                [page, sortIndex]
+              );
+
+              const animatedStyle = useAnimatedStyle(() => {
+                return {
+                  transform: [
+                    {
+                      translateX: translateX.value + x.value + offset.value,
+                    },
+                  ],
+                };
+              });
+              return (
+                <Animated.View key={id} style={[styles.page, animatedStyle]}>
+                  <RenderItem pageIndex={id} />
+                  <Text style={styles.text}>ID: {id}</Text>
+                  <Text style={styles.text}>Sort Index: {sortIndex}</Text>
+                  <Text style={styles.text}>Current Page: {currentPage}</Text>
+                </Animated.View>
+              );
+            }
+          )}
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 }
 
@@ -150,7 +179,6 @@ const styles = StyleSheet.create({
     width: CAROUSEL_WIDTH,
     height: "100%",
     alignItems: "center",
-    justifyContent: "center",
     flexDirection: "row",
     gap: CARD_GAP,
   },
@@ -163,7 +191,6 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     width: CARD_WIDTH,
     height: "80%",
-    borderColor: "black",
   },
 });
 
